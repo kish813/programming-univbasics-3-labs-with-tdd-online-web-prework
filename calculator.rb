9a79d0e21846b18313c5d8affc9666c5202c144a
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-22 23:25:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,4 @@
 ./calculator.rb
 contains a local variable called first_number that is assigned to a number
 expect(second_number).not_to equal (0)
+contains a local variable named sum = first_number + second_number

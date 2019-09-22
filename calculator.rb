ce3f46c1af6a9590321b17aadbd150f3009af870
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-22 23:27:29 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,4 @@ contains a local variable called first_number that is assigned to a number
 expect(second_number).not_to equal (0)
 contains a local variable named sum = first_number + second_number
 contains a local variable named difference = first_number - second_number
+contains a local variable named product = first_number * second_number
